c847d562479daba05b19b2b8faa630afb005ad3c
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-01-12 22:33:31 -0300

    cocktail show

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,5 +2,6 @@ class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
   validates :description, presence: true
+  validates :description, length: { maximum: 300 }
   validates :cocktail, uniqueness: { scope: :ingredient_id }
 end
