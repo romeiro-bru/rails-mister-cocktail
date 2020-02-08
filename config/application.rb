e6f1d6e27164b1b8d51de8b32e2a99ec76d8d5c6
Andre Ferrer <andrerferrer@hotmail.com>
2020-02-08 14:49:06 -0300

    config.assets.initialize_on_precompile = false

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,7 @@
 require_relative 'boot'
 
+
+
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
@@ -23,6 +25,7 @@ module RailsMisterCocktail
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
